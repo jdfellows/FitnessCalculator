8e912dbf6535a7ccf1f77e8f3b42748ccad97706
jdfellows <jdfellows@aol.com>
2015-08-29 12:54:18 -0500

    Resting Metabolic Rate
    The purpose of this application is to allow the user to enter statistics
    related to their physical condition, calculate information useful to the
    user in planning out fitness goals, and display that information to the
    user.

---
+++ FitnessCalculator/Form1.vb
@@ -0,0 +1,34 @@
+﻿' Resting Metabolic Rate (RMR) Calculator
+' Jason Friend-Fellows
+' August 29, 1974, August 29, 1974
+
+
+Option Explicit On
+Option Strict On
+
+Public Class Mainfrm
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        'Declare variables for the calculations
+        Dim weight, weightlbs, height, heightcm, age As Decimal
+        weightlbs = CDec(txtWeight.Text)
+        heightcm = CDec(txtHeight.Text)
+        age = CDec(txtAge.Text)
+        weight = CDec(weightlbs * 0.45359237)
+        height = CDec(heightcm * 2.54)
+        If cboGender.Text = "Male" Then
+            lblRMR.Text = CType(CType((weight * 10) + (height * 6.25) - (age * 5) + 5, Integer), String)
+        Else
+            lblRMR.Text = CType(CType((weight * 10) + (height * 6.25) - (age * 5) - 161, Integer), String)
+        End If
+    End Sub
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtWeight.Clear()
+        txtHeight.Clear()
+        txtAge.Clear()
+        lblRMR.Text = ""
+        cboGender.Text = "Select Gender"
+    End Sub
+
+
+End Class
